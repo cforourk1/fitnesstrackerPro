033d5427ba7ebe289883d3a9fb038e6850e050ef
Crystal <cforourk@umich.edu>
2026-05-07 15:15:48 -0400

    set form is hell

---
+++ src/routines/SetForm.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import { useAuth } from "../auth/AuthContext";
+import { createSet } from "../api/routines";
+import { getActivities } from "../api/activities";
+
+/** Form for a user to create a new Set with a name and goal. */
+export default function SetForm({ syncSets }) {
+const [activities, setActivities] = useState([]);
+  const { token } = useAuth();
+
+  const [error, setError] = useState(null);
+useEffect(() => {
+  const loadActivities = async () => {
+    const data = await getActivities();
+    setActivities(data);
+  };
+  loadActivities();
+}, []);
+const tryCreateSet = async (formData) => {
+  setError(null);
+  const activityId = Number(formData.get("activityId"));
+  const routineId = Number(formData.get("routineId"));
+  const count = Number(formData.get("count"));
+
+  try {
+    await createSet(token, { activityId, routineId, count });
+    syncSets();
+  } catch (e) {
+    setError(e.message);
+  }
+};
+
+  return (
+    <>
+      <h2>Add a new Set</h2>
+     <form action={tryCreateSet}>
+  <label>
+    Activity
+    <select name="activityId">
+      {activities.map((activity) => (
+        <option key={activity.id} value={activity.id}>
+          {activity.name}
+        </option>
+      ))}
+    </select>
+  </label>
+  <label>
+    Count
+    <input type="number" name="count" />
+  </label>
+  <button>Add Set</button>
+</form>
+      {error && <p role="alert">{error}</p>}
+    </>
+  );
+}
+
