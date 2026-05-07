7846a9e598158d947fe3522111f0b5cbc7083864
Crystal <cforourk@umich.edu>
2026-05-07 15:18:47 -0400

    added auth to routine page

--- src/routines/RoutinePage.jsx
+++ src/routines/RoutinePage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
 import { deleteRoutine, getRoutines } from "../api/routines";
 import { useAuth } from "../auth/AuthContext";
+import  SetForm from "./SetForm";
 /** useParams is a hook that looks at the current url and hands back the dynamic parts., this is how *we will be able to identify which Routine we are on. That will display in the URL. And that is how *we will be able to navigate to a specific Routine rather than the entire list of activities.
  * */
 export default function RoutinePage() {
@@ -52,6 +53,7 @@ export default function RoutinePage() {
               ))}
             </ul>
           )}
+          {token && <SetForm routineId={Routine.id} syncSets={syncRoutine} />}
           {token && <button onClick={tryDelete}>Delete</button>}
           {error && <p role="alert">{error}</p>}
         </div>
