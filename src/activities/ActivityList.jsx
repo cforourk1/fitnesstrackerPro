2345235e7b83bab6b95773f4e1815a6c28074e30
Crystal <cforourk@umich.edu>
2026-05-07 12:35:00 -0400

    added link to activity id and removed a tag

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { deleteActivity } from "../api/activities";
 import { useAuth } from "../auth/AuthContext";
+import { Link } from "react-router";
 
 export default function ActivityList({ activities, syncActivities }) {
   return (
@@ -31,10 +32,10 @@ function ActivityListItem({ activity, syncActivities }) {
       setError(e.message);
     }
   };
-
+//added link to for the activity ID 
   return (
     <li>
-      <p>{activity.name}</p>
+      <Link to={`/activities/${activity.id}`}>{activity.name}</Link>
       {token && <button onClick={tryDelete}>Delete</button>}
       {error && <p role="alert">{error}</p>}
     </li>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import { Outlet } from "react-router"
 
 /** The shared layout for all pages of the app
- * outlet is how react router tells the layout wrapper to render all of the child routes. 
+ * outlet is how react router tells the layout wrapper to render all of the child routes.
  */
 export default function Layout() {
   return (
