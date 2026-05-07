6632dbe9cbdc6efea429b52302d51661bc32c877
Crystal <cforourk@umich.edu>
2026-05-07 15:28:36 -0400

    delete buttons for sets added

--- src/routines/RoutinePage.jsx
+++ src/routines/RoutinePage.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
-import { deleteRoutine, getRoutines } from "../api/routines";
+import { deleteRoutine, getRoutines, deleteSet} from "../api/routines";
 import { useAuth } from "../auth/AuthContext";
-import  SetForm from "./SetForm";
+import SetForm from "./SetForm";
 /** useParams is a hook that looks at the current url and hands back the dynamic parts., this is how *we will be able to identify which Routine we are on. That will display in the URL. And that is how *we will be able to navigate to a specific Routine rather than the entire list of activities.
  * */
 export default function RoutinePage() {
@@ -49,6 +49,13 @@ export default function RoutinePage() {
               {Routine.sets.map((set) => (
                 <li key={set.id}>
                   {set.name} x {set.count}
+                  {token && (
+                    <button
+                      onClick={() => deleteSet(token, set.id).then(syncRoutine)}
+                    >
+                      Delete Set
+                    </button>
+                  )}
                 </li>
               ))}
             </ul>
